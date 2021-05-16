ee0f031c1d69274c7c5701b58c0a0a4d5ae9a15d
Sean Lars Ganser <sean@meetingpulse.net>
2021-05-15 21:07:46 -0400

    updated for v15

--- main.tf
+++ main.tf
@@ -36,11 +36,27 @@ data "aws_route53_zone" "zone" {
 }
 
 resource "aws_route53_record" "cert_validation" {
-  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.zone.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options: dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.zone.id
+
+
+  # deprecated < v13
+  # name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
+  # type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
+  # zone_id = "${data.aws_route53_zone.zone.id}"
+  # records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  # ttl     = 60
 }
 
 # This isn't a real "thing" in AWS. This is just here to make sure that terraform
